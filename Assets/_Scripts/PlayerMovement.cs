cc2a54b8b5979e4b9e7c979def49cb24a1329964
Ntv2201 <vubadaolop6e@gmail.com>
2026-09-14 21:35:14 +0700

    import character

--- Assets/_Scripts/PlayerMovement.cs
+++ Assets/_Scripts/PlayerMovement.cs
@@ -2,5 +2,28 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    
+    [SerializeField] private float playerSpeed = 2f;
+    [SerializeField] private float horizontalSpeed = 3f;
+    [SerializeField] private float rightLimit = 5.5f;
+    [SerializeField] private float leftLimit = -5.5f;
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * Time.deltaTime * playerSpeed, Space.World);
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            if(this.gameObject.transform.position.x > leftLimit)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
+            }
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            if(this.gameObject.transform.position.x < rightLimit)
+            {
+                transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed * -1);
+            }
+        }
+    }   
 }
